4c4ac278d87368d4d805b2c206372f3205a127ac
rajuahmmed <rajuahmmed.cse@gmail.com>
2016-02-04 07:54:47 +0000

    [c9] Work

---
+++ core/fields/fields_views/dimension.php
@@ -0,0 +1,24 @@
+        <#
+        var miniAttr = '';
+        if (! _.isUndefined(data.choices[ 'min' ]) ){
+        	miniAttr = 'min="'+data.choices[ 'min' ]+'"';
+        }
+
+        var maxAttr = '';
+        if (! _.isUndefined(data.choices[ 'max' ]) ){
+        	maxAttr = 'max="'+data.choices[ 'max' ]+'"';
+        }
+
+        var stepAttr = '';
+        if (! _.isUndefined(data.choices[ 'step' ]) ){
+        	stepAttr = 'data-step="'+data.choices[ 'step' ]+'"';
+        }
+        #>
+        <div class="ctf-input-field ctf-input-field-number">
+          <input type="number" value="{{ data.value }}" {{{ miniAttr }}} {{{ maxAttr }}} {{{ stepAttr }}} {{{ data.link }}}>
+          <select>
+      			<# for ( key in data.choices['units'] ) { #>
+      				<option value="{{ key }}"<# if ( key === data.value ) { #>selected<# } #>>{{ data.choices['units'][ key ] }}</option>
+      			<# } #>
+      		</select>
+        </div>
