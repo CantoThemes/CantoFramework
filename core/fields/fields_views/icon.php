19a3d4eb540028be454cb97c4cdc6295df3b215c
rajuahmmed <rajuahmmed.cse@gmail.com>
2016-02-20 13:43:37 +0000

    API Improved

--- core/fields/fields_views/icon.php
+++ core/fields/fields_views/icon.php
@@ -3,11 +3,11 @@
           <div class="ct-iconpicker">
             <div class="ct-ip-holder">
               <div class="ct-ip-icon"><i class="{{ data.value }}"></i></div>
-              <input type="hidden" value="{{ data.value }}" class="ct-icon-value">
+              <input type="hidden" value="{{ data.value }}" class="ct-icon-value" {{{ data.link }}} />
             </div>
             <div class="ct-ip-popup clearfix">
               <div class="ct-ip-search">
-                <input type="text" class="ct-ip-search-input" placeholder="Search icon" {{{ data.link }}} />
+                <input type="text" class="ct-ip-search-input" placeholder="Search icon" />
               </div>
               <ul>
                 <# for ( key in ctf_fa_icons ) { #>
