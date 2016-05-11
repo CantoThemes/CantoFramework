6ae5b7cafa70a57576abe0b0a87c1971157296ac
rajuahmmed <rajuahmmed.cse@gmail.com>
2016-05-11 15:43:08 +0000

    Styling...

--- core/fields/fields_views/text_multi.php
+++ core/fields/fields_views/text_multi.php
@@ -7,7 +7,7 @@
 					</div>
 				<# } #>
 			</div>
-			<button class="ctf-mt-add-new ctf-btn" data-name="{{{ _.escape(data.link) }}}">Add New</button>
+			<button class="ctf-mt-add-new ctf-btn ctf-btn-small" data-name="{{{ _.escape(data.link) }}}"><i class="fa fa-plus"></i> Add</button>
 			<div class="ctf-mt-input-item ctf-hidden ctf-mt-tmpl">
 				<input type="text" class="ctf-txt-field" >
 				<button class="ctf-mt-input-delete">x</button>
